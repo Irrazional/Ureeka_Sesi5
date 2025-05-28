198e5368c827d5922b00d719d2874d0aa9e8f58a
Irrazional <70615953+Irrazional@users.noreply.github.com>
2025-05-28 14:39:48 +0700

    added Clerk for Login Page

--- sesi5/src/app/page.tsx
+++ sesi5/src/app/page.tsx
@@ -2,31 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          className="border border-gray-300 rounded p-2 mb-4"
-        />
-
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="border border-gray-300 rounded p-2 mb-4"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white rounded p-2 hover:bg-blue-600"
-        >
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="border border-gray-300 rounded p-2 mb-4"
-          />
-          Submit
-        </button>
-      </form>
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="text-center px-4">
+        <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800">
+          Click the button on the top right to sign in or sign up
+        </h1>
+      </div>
     </main>
   );
 }
